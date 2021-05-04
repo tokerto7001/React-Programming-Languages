116780eb89880d083c0535a1a0d46abcbb38e61b
Hasan TOKER <75892548+tokerto7001@users.noreply.github.com>
2021-05-05 00:53:23 +0300

    Adding transition skills

--- react-programming-languages/src/components/Card.jsx
+++ react-programming-languages/src/components/Card.jsx
@@ -1,16 +1,19 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import logo from "./react.svg"
 import { categories } from "./data"
 import Cards from './Cards'
 
+
 export default function Card() {
     
-   
+   useEffect(() => (
+       document.querySelector(".react-image").className = "react-image-reverse"
+   ), [])
 
     return (
         <div>
             <img src={logo} className="react-image" />
-
+            
             <div className="card-div">
 
                 <div className="stripe"></div>
